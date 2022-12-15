f28aba3c722a4eda2ece529dd8fb63c58796cd37
iknsaeyn <iknsaeyn@gmail.com>
2022-12-15 11:12:05 +0300

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
+import { ItemComponent } from './item/item.component';
+import { ItemDetailsComponent } from './item/item-details/item-details.component';
+import { ItemListComponent } from './item/item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    AboutComponent,
+    MainComponent,
+    ItemComponent,
+    ItemDetailsComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent {
+}
