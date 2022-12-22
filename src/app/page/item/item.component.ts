25e8e21bd011ac4d1f230c811260d2bf27c6f72f
iknsaeyn <iknsaeyn@gmail.com>
2022-12-22 16:57:56 +0300

    +

---
+++ src/app/page/item/item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent {
+
+}
