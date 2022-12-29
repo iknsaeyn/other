95475d6ed0c89c0b7e5d0d125c12ed2761e4f978
iknsaeyn <iknsaeyn@gmail.com>
2022-12-29 14:34:24 +0300

    +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'other';
+export class AppComponent implements OnInit {
+  constructor(public title: Title, private meta: Meta) {
+    this.meta.addTag({ property: 'og:desc', content: 'root_desc' })
+  }
+  // property, потому что, как я понял, подрузумивается OpenGraph, хотя desc я в прмерах не нашел.
+
+  ngOnInit(): void {
+    this.title.setTitle('Practice 1.7')
+  }
 }
---
+++ src/app/inline/inline.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-inline',
+  templateUrl: './inline.component.html',
+  styleUrls: ['./inline.component.scss']
+})
+export class InlineComponent {
+  constructor(private meta: Meta) {
+    this.meta.addTags([
+      { property: 'og:title', content: 'The Rock' },
+      { property: 'og:type', content: 'video.movie' },
+      { property: 'og:url', content: '//www.imdb.com/title/tt0117500/' }
+    ])
+  }
+
+}
