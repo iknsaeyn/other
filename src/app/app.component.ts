d4741aa4d31fde3fab87bcbc9ebad752fe20bc81
iknsaeyn <iknsaeyn@gmail.com>
2023-01-19 18:12:29 +0300

    +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+interface Books {
+  title: string | null;
+  author: string | null;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'other';
+  books: Books[] = [{
+    title: 'ВойнаИМир',
+    author: 'ЛевТолстой'
+  },
+  {
+    title: 'МаленькийПринц',
+    author: 'АнтуанДеСентЭкзюпери'
+  }
+  ]
+
+  creat_book = {
+    title: null,
+    author: null
+  }
+
+  create() {
+    this.books.push({
+      title: this.creat_book.title,
+      author: this.creat_book.author
+    })
+  }
 }
