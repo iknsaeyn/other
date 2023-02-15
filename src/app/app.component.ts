1d3deb945489e68d4662d7c67302552ca5ce3fd1
iknsaeyn <iknsaeyn@gmail.com>
2023-02-16 01:08:15 +0300

    +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetService } from './_services/get.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'other';
+export class AppComponent implements OnInit {
+  constructor(private getService: GetService) { }
+
+  todos: object[] = []
+
+  ngOnInit() {
+    this.getService.getTodos().subscribe({
+      next: (todos: any) => {
+        this.todos = todos
+      },
+      error: () => console.log('Ошибка')
+    })
+  }
+
+
 }
