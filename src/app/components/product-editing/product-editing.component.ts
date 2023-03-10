b5ab30c16ea2f23c87956ec28270d7e22c0f92f6
iknsaeyn <iknsaeyn@gmail.com>
2023-03-11 01:49:41 +0300

    +

--- src/app/components/product-editing/product-editing.component.ts
+++ src/app/components/product-editing/product-editing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { BaseService } from 'src/app/services/base.service';
 
@@ -8,10 +8,23 @@ import { BaseService } from 'src/app/services/base.service';
   styleUrls: ['./product-editing.component.scss']
 })
 export class ProductEditingComponent implements OnInit {
+  @ViewChild('title') title!: ElementRef
+  @ViewChild('description') description!: ElementRef
+  @ViewChild('price') price!: ElementRef
+  @ViewChild('brand') brand!: ElementRef
+  @ViewChild('rating') rating!: ElementRef
   id: any = 0
   constructor(public baseService: BaseService, private activeRouter: ActivatedRoute) { }
 
   ngOnInit() {
     this.id = this.activeRouter.snapshot.paramMap.get('id')
+  }
+
+  change() {
+    this.baseService.arr[this.id - 1].title = this.title.nativeElement.value
+    this.baseService.arr[this.id - 1].description = this.description.nativeElement.value
+    this.baseService.arr[this.id - 1].price = this.price.nativeElement.value
+    this.baseService.arr[this.id - 1].brand = this.brand.nativeElement.value
+    this.baseService.arr[this.id - 1].rating = this.rating.nativeElement.value
   }
 }
